1c543c693f90cc5901a3327d26247f39f35ab02e
timothydgreer <timothydgreer@gmail.com>
2017-08-02 17:28:14 -0700

    Create get_funny_utterances.m

---
+++ get_funny_utterances.m
@@ -0,0 +1,120 @@
+A = importdata('subtitles/Friends_8/csv09_The_One_With_the_Rumor_(HD).txt');
+%Delimit
+utterance = cell(length(A),1);
+for i = 1:length(A)
+    first = strsplit(A{i},'|');
+    utterance{i} = first{1};
+    time_unsplit = first{2};
+    time_split = strsplit(time_unsplit,'^');
+    ep9utstart(i) = str2num(time_split{1});
+    ep9utend(i) = str2num(time_split{2});
+end
+ep9utstart = ep9utstart'; 
+ep9utend = ep9utend'; 
+
+
+window = 100;
+%RENAME FUNCTION!
+%[Xep9,Ytruthep8,NANs9,MFCCs_to_Look_At9] = get_data_single_episode_with_laugh_tags(ep1start, ep1end, yep1, 48000, window);
+%temp_svm = svmfriends.Trained{5};
+%Ypredict9 = predict(temp_svm,Xep9);
+
+Ysmooth9 = post_smooth(Ypredict9,1);
+
+LaughterEp9 = find_laugh_start(Ysmooth9,2);
+
+
+%Finds the indices of the NANs that are not zero. These correspond to
+%When there is a number in MFCCs, and this is where there is Y = 0 or 1
+NAN_zeros9 = find(NANs9 == 0); 
+
+%THERES NOTHING WRONG WITH THESE THREE CALLS
+laugh_start_times9 = NAN_zeros9(find(LaughterEp9 == 1))/10;
+
+utpred9 = cell(length(laugh_start_times9),1);
+for i = 1:length(laugh_start_times9)
+    for j = 1:length(ep9utstart)
+        if (ep9utstart(j) > laugh_start_times9(i))% && abs(ep9utend(j-1) - laugh_start_times9(i)) < 3)
+            utpred9{j-1} = utterance{j-1};
+            break
+        else
+            continue
+        end
+    end
+end
+
+laugh_start_times_truth9 = round(ep9start,1);
+%smooth the ep1start to account for l --> ls and vv
+
+laugh_end_times_truth9 = round(ep9end,1);
+ep9real_start = ep9start(1);
+for i = 2:length(laugh_end_times_truth9)
+    if laugh_start_times_truth9(i) == laugh_end_times_truth9(i-1);
+        ;
+    else
+        ep9real_start = [ep9real_start, laugh_start_times_truth9(i)];
+    end
+end
+
+%work on this to find endtimes
+% ep9real_end = ep9end(end);
+% flipl
+% for i = 2:length(laugh_end_times_truth9)
+%     if laugh_start_times_truth9(i) == laugh_end_times_truth9(i-1);
+%         ;
+%     else
+%         ep9real_start = [ep9real_start, laugh_start_times_truth9(i)];
+%     end
+% end
+% 
+% ep9real_end = flip(ep9real_end);
+
+utact9 = cell(length(ep9real_start),1);
+for i = 1:length(ep9real_start)
+    for j = 1:length(ep9utstart)
+        if (ep9utstart(j) > ep9real_start(i))% && abs(ep9utstart(j) - ep9real_start(i)) < 4)
+            temp_str = utterance{j-1}
+            %DATA SPECIFIC :(
+            if ~isempty(regexp(temp_str, '[OA](kay|h)(\W+)?$', 'match'))
+                h = 1
+                utact9{j-1} = utterance{j-2};
+            else
+                utact9{j-1} = utterance{j-1};
+            end
+            break
+        else
+            continue
+        end
+    end
+end
+countpred = 0;
+countact = 0;
+for i = 1:length(utpred9)
+    if ~isempty(utpred9{i})
+        countpred = countpred+1;
+    end
+end
+
+for i = 1:length(utact9)
+    if ~isempty(utact9{i})
+        countact = countact+1;
+    end
+end
+
+utpreddist9 = cell(length(countpred),1);
+utactdist9 = cell(length(countact),1);
+countpred = 1;
+countact = 1;
+for i = 1:length(utpred9)
+    if ~isempty(utpred9{i})
+        utpreddist9{countpred} = utpred9{i};
+        countpred = countpred+1;
+    end
+end
+
+for i = 1:length(utact9)
+    if ~isempty(utact9{i})
+        utactdist9{countact} = utact9{i};
+        countact = countact+1;
+    end
+end
