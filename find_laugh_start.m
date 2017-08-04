03d1a6f5219dc871a2cb568749e55ac5df57c2f7
timothydgreer <timothydgreer@gmail.com>
2017-08-03 17:00:21 -0700

    Update find_laugh_start.m

--- find_laugh_start.m
+++ find_laugh_start.m
@@ -1,13 +1,21 @@
 function [laugh_start] = find_laugh_start(Y,thresh)
+    %grabs the first window of every section of laughter that is long
+    %enough to meet the threshold
     laugh_start = zeros(1,length(Y));
     i = 1;
+    %scans through all windows of laughter
     while i < (length(Y))
+        %finds a new section of laughter
         if(Y(i) == 1)
             starti = i;
             count = 1;
+            %counts every subsequent window of laughter in a section of laughter
             while (i+1 < length(Y) && Y(i+1) == 1)
                 i = i + 1;
                 count = count +1;
+                %if the number of windows of consecutive laughter in a section
+                %meets the threshold then the first window of the section is
+                %recorded
                 if(count == thresh)
                     laugh_start(starti) = 1;
                 end
@@ -17,5 +25,6 @@ function [laugh_start] = find_laugh_start(Y,thresh)
             i = i+1;
         end
     end
+    %lazy transpose
     laugh_start = laugh_start';
 end
