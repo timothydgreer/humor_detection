811501617c0e4ff0bee1db884eb7d88570196b7b
timothydgreer <timothydgreer@gmail.com>
2017-08-03 17:26:09 -0700

    Update make_max_thresh.m

--- make_max_thresh.m
+++ make_max_thresh.m
@@ -2,25 +2,34 @@ function [max, best_thresh] = make_max_thresh(svmfull,Y_full_f_h)
     %Make ROC Curve for a single episode
     fs = 48000;
     window = 100;
-    %[X,Y] = get_data_single_episode(ep1start, ep1end, yep1, fs, window);
-    %svm = fitcsvm(X,Y,'Standardize',true,'KernelFunction','RBF','KernelScale','auto','CrossVal','on');
+    %Prediction used to find score (likelihood) to be used later
     [Y2, scores] = kfoldPredict(svmfull);
+    %precision and recall
     prec = [];
     rec = [];
     max = 0;
+    %sweep threshold
     for i = -3:.05:3
+    %grabs the first score (likelhood)
         Ytemp = double((scores(:,1) < i));
+        %Gets precision, recall, and f1 scores for this threshold by comparing
+        %scores below threshold and truth data
         [temp_prec, temp_rec, temp_f] = getPRF(Ytemp,Y_full_f_h);
+        %records precision for this threshold
         prec = [prec, temp_prec];
+        %records recall for this threshold
         rec = [rec, temp_rec];
+        %updates max f1 score
         if temp_f > max
             max = temp_f;
             best_thresh = i;
         end
     end
+    %plot
     figure
     plot(rec, prec);   
     hold off
+    %reimman sum to find the AUC
     height = (prec(2:end)+prec(1:end-1))/2;
     width = (rec(2:end)-rec(1:end-1));
     auc = sum(height.*width)
